8ba402ab55f428c0d76fad55e14ab2ec6b2bdce8
harish <yarraguntaharish93@gmail.com>
2023-01-25 13:55:39 +0530

    made changes

--- ROLES_MODULE/variables.tf
+++ ROLES_MODULE/variables.tf
@@ -13,3 +13,26 @@ variable "role_name" {
 variable "users" {
   type = list(string)
 }
+
+
+variable "roles1" {
+  type = map(object({
+          role_name= string,
+          roles = list(string),
+          users= list(string)
+  }))
+  default = {
+    "one" = {
+      role_name = "admin9"
+      roles = [ "SYSADMIN" ]
+      #users = [module.ALL_USERS_DEV001.USERS.snowflake_user30.name,]
+    }
+  }
+}
+
+resource "snowflake_role_grants" "ROLE_GRANTS" {
+  for_each = var.roles1
+  role_name = each.value["role_name"]
+  roles = each.value["roles"]
+  users = [module.ALL_USERS_DEV001.USERS.snowflake_user30.name]
+}
